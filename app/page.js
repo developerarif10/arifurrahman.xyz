643509c05ff693f79f30ed0b550ea37199937ce2
Arifur Rahman <90108739+developerarif10@users.noreply.github.com>
2025-01-13 23:56:14 +0600

    header changes

--- app/page.js
+++ app/page.js
@@ -1,9 +1,31 @@
+import Header from "@/components/header/header";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div>Arifur Rahman</div>
-      </main>
+    <div id="webcrumbs">
+      <div className=" text-neutral-50 rounded-lg shadow-lg p-8">
+        <Header />
+        <main className="">
+          <h1 className="text-4xl font-title mb-4">
+            I&apos;m a <span>software engineer</span> that
+            <br />
+            rarely{" "}
+            <span className="text-primary-500 font-title">writes code.</span>
+          </h1>
+          <p className="text-neutral-300 leading-7 mb-6">
+            Meet Manu Arora, the self-proclaimed code wizard who can turn
+            caffeine into beautiful websites. His passion for building clean and
+            functional designs is only rivaled by his passion for finding the
+            perfect GIF to express his excitement.
+          </p>
+          <p>
+            Building{" "}
+            <span className="font-medium text-neutral-50">Algochurn</span> and{" "}
+            <span className="font-medium text-neutral-50">Aceternity</span> when
+            Im not working on my day job.
+          </p>
+        </main>
+      </div>
     </div>
   );
 }
---
+++ components/header/header.jsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import Link from "next/link";
+import Button from "../my-button";
+
+export default function Header() {
+  return (
+    <header className="flex items-center justify-between mb-10">
+      <nav className="flex items-center gap-4">
+        <div className="flex items-center gap-2 bg-neutral-800 rounded-full px-4 py-2">
+          <Image
+            width={100}
+            height={80}
+            src="https://tools-api.webcrumbs.org/image-placeholder/40/40/avatars/1"
+            alt="avatar"
+            className="w-[40px] h-[40px] rounded-full object-cover"
+          />
+          <span className="font-medium">Dev Arifur</span>
+        </div>
+        <ul className="flex items-center gap-6">
+          <li>
+            <Link href="#" className="hover:underline">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="#" className="hover:underline">
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link href="#" className="hover:underline">
+              Contributions
+            </Link>
+          </li>
+          <li>
+            <Link href="#" className="hover:underline">
+              Blogs
+            </Link>
+          </li>
+          <li>
+            <Link href="#" className="hover:underline">
+              Events
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Button className="bg-neutral-700 hover:bg-neutral-600 text-neutral-50 rounded-md px-5 py-2">
+        Download CV
+      </Button>
+    </header>
+  );
+}
