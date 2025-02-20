3951571ea54adf3059fb491e49701eeaf11fdceb
Arifur Rahman <90108739+developerarif10@users.noreply.github.com>
2025-02-21 01:09:34 +0600

    project page changes

---
+++ app/projects/_components/project-card.jsx
@@ -0,0 +1,51 @@
+import { ArrowUpRight, CodeXml } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function ProjectCard({ thumbnail }) {
+  return (
+    <div className="p-3 lg:w-1/2 md:w-full">
+      <div className="relative">
+        <Image
+          src={`/projects/project-${thumbnail}.webp`}
+          alt={`project ${thumbnail}`}
+          className="w-full h-48 md:h-72 object-cover group-hover:scale-105 transition-transform duration-300 rounded-xl"
+          width={500}
+          height={300}
+        />
+        <div className="absolute bottom-0 left-0 right-0 h-1/5 bg-gradient-to-t from-black/80 to-transparent p-2 flex gap-2 items-end">
+          <span className="bg-white/10 text-white text-sm px-3 py-1 rounded-full">
+            React
+          </span>
+          <span className="bg-white/10 text-white text-sm px-3 py-1 rounded-full">
+            Next.js
+          </span>
+        </div>
+      </div>
+      <div className="flex justify-between m-4">
+        <div>
+          <h2 className=" text-lg title-font font-medium">Svgl.app</h2>
+          <p className="leading-relaxed text-base">Contributor</p>
+        </div>
+        <div className="flex gap-2">
+          <Link
+            target="_blank"
+            href="#"
+            aria-label="Preview"
+            className="box-border text-[rgba(243,243,243,0.596)] flex h-[46px] w-[46px] items-center justify-center bg-[rgba(20,20,20,0.61)] duration-[0.3s] transition-[color,background-color,border-color,text-decoration-color,fill,stroke,opacity,box-shadow,transform,filter,backdrop-filter,-webkit-backdrop-filter] ease-in-out leading-[24px] p-[12px] rounded-[12px] border-[0.666667px] border-solid border-[rgba(243,243,243,0.063)]"
+          >
+            <CodeXml />
+          </Link>
+          <Link
+            target="_blank"
+            href="#"
+            aria-label="Preview"
+            className="box-border text-[rgba(243,243,243,0.596)] flex h-[46px] w-[46px] items-center justify-center bg-[rgba(20,20,20,0.61)] duration-[0.3s] transition-[color,background-color,border-color,text-decoration-color,fill,stroke,opacity,box-shadow,transform,filter,backdrop-filter,-webkit-backdrop-filter] ease-in-out leading-[24px] p-[12px] rounded-[12px] border-[0.666667px] border-solid border-[rgba(243,243,243,0.063)]"
+          >
+            <ArrowUpRight />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/projects/page.js
+++ app/projects/page.js
@@ -1,5 +1,5 @@
-import { ArrowUpRight, CodeXml } from "lucide-react";
-import Image from "next/image";
+import Link from "next/link";
+import ProjectCard from "./_components/project-card";
 
 export default function ProjectPage() {
   return (
@@ -11,54 +11,31 @@ export default function ProjectPage() {
           pretty pleased with. Most, if not all of them, were built for fun.
         </p>
       </div>
-      <div className="my-10">
+      <div className="my-5">
         <div>
-          <h1>Full Stack Projects</h1>
-          <div className="container px-5 py-24 mx-auto flex flex-wrap">
+          <h1 className="text-2xl font-semibold">Full Stack Projects</h1>
+          <div className="container px-5 py-12 flex flex-wrap justify-center items-center">
             <div className="flex flex-wrap -m-4">
-              <div className="p-4 lg:w-1/2 md:w-full">
-                <Image
-                  src="/projects/project-1.webp"
-                  alt="project 1"
-                  className="w-full h-48 md:h-72 object-cover group-hover:scale-105 transition-transform duration-300"
-                  width={300}
-                  height={300}
-                />
-                <div className="flex justify-between m-4">
-                  <div>
-                    <h2 className=" text-lg title-font font-medium">
-                      Svgl.app
-                    </h2>
-                    <p className="leading-relaxed text-base">Contributor</p>
-                  </div>
-                  <div className="flex gap-2">
-                    <CodeXml />
-                    <ArrowUpRight />
-                  </div>
-                </div>
-              </div>
-              <div className="p-4 lg:w-1/2 md:w-full">
-                <Image
-                  src="/projects/project-2.webp"
-                  alt="project 1"
-                  className="w-full h-48 md:h-72 object-cover group-hover:scale-105 transition-transform duration-300"
-                  width={300}
-                  height={300}
-                />
-                <div className="flex justify-between m-4">
-                  <div>
-                    <h2 className=" text-lg title-font font-medium">
-                      Svgl.app
-                    </h2>
-                    <p className="leading-relaxed text-base">Contributor</p>
-                  </div>
-                  <div className="flex gap-2">
-                    <CodeXml />
-                    <ArrowUpRight />
-                  </div>
-                </div>
-              </div>
+              <ProjectCard thumbnail={1} />
+              <ProjectCard thumbnail={2} />
             </div>
+          </div>
+          <div className="flex justify-center items-center text-center">
+            <Link
+              href="https://github.com/developerarif10"
+              target="_blank"
+              aria-label="GitHub"
+              className="cursor-pointer text-zinc-200 flex gap-2 border border-black items-center bg-black px-4 py-2 rounded-lg font-medium text-sm hover:bg-[#111111] hover:border transition-all ease-in duration-200"
+            >
+              More projects on
+              <svg
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 fill-zinc-200"
+              >
+                <path d="M10.9,2.1c-4.6,0.5-8.3,4.2-8.8,8.7c-0.6,5,2.5,9.3,6.9,10.7v-2.3c0,0-0.4,0.1-0.9,0.1c-1.4,0-2-1.2-2.1-1.9 c-0.1-0.4-0.3-0.7-0.6-1C5.1,16.3,5,16.3,5,16.2C5,16,5.3,16,5.4,16c0.6,0,1.1,0.7,1.3,1c0.5,0.8,1.1,1,1.4,1c0.4,0,0.7-0.1,0.9-0.2 c0.1-0.7,0.4-1.4,1-1.8c-2.3-0.5-4-1.8-4-4c0-1.1,0.5-2.2,1.2-3C7.1,8.8,7,8.3,7,7.6C7,7.2,7,6.6,7.3,6c0,0,1.4,0,2.8,1.3 C10.6,7.1,11.3,7,12,7s1.4,0.1,2,0.3C15.3,6,16.8,6,16.8,6C17,6.6,17,7.2,17,7.6c0,0.8-0.1,1.2-0.2,1.4c0.7,0.8,1.2,1.8,1.2,3 c0,2.2-1.7,3.5-4,4c0.6,0.5,1,1.4,1,2.3v3.3c4.1-1.3,7-5.1,7-9.5C22,6.1,16.9,1.4,10.9,2.1z" />
+              </svg>
+            </Link>
           </div>
         </div>
       </div>
