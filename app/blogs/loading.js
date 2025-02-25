4491ab3ec89389972638ec1861613dde97f8ca39
Arifur Rahman <90108739+developerarif10@users.noreply.github.com>
2025-02-25 23:24:38 +0600

    Loading, about and project page layout changes

--- app/blogs/loading.js
+++ app/blogs/loading.js
@@ -2,15 +2,19 @@ import { TextShimmerWave } from "@/components/ui/text-shimmer-wave";
 
 export default function loading() {
   return (
-    <TextShimmerWave
-      className="[--base-color:#0D74CE] [--base-gradient-color:#5EB1EF]"
-      duration={1}
-      spread={1}
-      zDistance={1}
-      scaleDistance={1.1}
-      rotateYDistance={20}
-    >
-      Loading...
-    </TextShimmerWave>
+    <div className="min-h-screen p-6">
+      <div className="max-w-4xl mx-auto">
+        <TextShimmerWave
+          className="[--base-color:#0D74CE] [--base-gradient-color:#5EB1EF]"
+          duration={1}
+          spread={1}
+          zDistance={1}
+          scaleDistance={1.1}
+          rotateYDistance={20}
+        >
+          Loading...
+        </TextShimmerWave>
+      </div>
+    </div>
   );
 }
