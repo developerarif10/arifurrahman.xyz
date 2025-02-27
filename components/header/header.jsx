8c6f3786c8ff6b9cb5985c670a7a5cfb0f1c6a76
Arifur Rahman <90108739+developerarif10@users.noreply.github.com>
2025-02-27 18:24:31 +0600

    Navbar changes

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -2,7 +2,7 @@ import Navbar from "./navbar";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 inset-x-0 flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full text-sm">
+    <header className="fixed top-0 left-0 w-full z-50">
       <Navbar />
     </header>
   );
