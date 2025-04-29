da35032a0fb3992c085dec0f50398b7230b81099
Arifur Rahman <90108739+developerarif10@users.noreply.github.com>
2025-04-29 20:45:04 +0600

    Hero section updated

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header/header";
 import { ThemeProvider } from "@/providers/theme-provider";
+import { ReactLenis } from "lenis/react";
 import { Source_Sans_3 } from "next/font/google";
 import "./globals.css";
 
@@ -17,20 +18,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${sourceSansPro.className} antialiased`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <section>
-            <Header />
-            <main>{children}</main>
-            <Footer />
-          </section>
-        </ThemeProvider>
-      </body>
+      <ReactLenis root>
+        <body className={`${sourceSansPro.className} antialiased`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <section>
+              <Header />
+              <main>{children}</main>
+              <Footer />
+            </section>
+          </ThemeProvider>
+        </body>
+      </ReactLenis>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,21 +1,28 @@
 "use client";
+
 import Card from "@/components/cards/card";
 import ContactCard from "@/components/cards/contact-card";
 import Hero from "@/components/hero-section";
+import StackingPage from "@/components/StackingCard";
 import TesitomonialPage from "@/components/tesitomonial-page";
+import { ReactLenis } from "lenis/react";
 
 export default function Home() {
   return (
     <>
-      <Hero />
-      {/* Render Multiple Cards */}
-      <div className="min-h-screen p-6">
-        <div className="max-w-4xl mx-auto">
-          <Card />
-          <TesitomonialPage />
-          <ContactCard />
+      <ReactLenis root>
+        <Hero />
+
+        {/* Render Multiple Cards */}
+        <div className="min-h-screen p-6">
+          <div className="max-w-4xl mx-auto">
+            <Card />
+            <StackingPage />
+            <TesitomonialPage />
+            <ContactCard />
+          </div>
         </div>
-      </div>
+      </ReactLenis>
     </>
   );
 }
