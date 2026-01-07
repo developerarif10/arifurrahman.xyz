23dcd088f06a29e4b2a173f6db48f96800d69a58
Arifur Rahman <90108739+developerarif10@users.noreply.github.com>
2026-01-08 01:41:45 +0600

    Update The hero section & improvde

--- components/new-hero.jsx
+++ components/new-hero.jsx
@@ -7,48 +7,42 @@ import Link from "next/link";
 
 export default function NewHero() {
   return (
-    <section className="relative h-[100vh] w-full bg-white flex flex-col pt-24 pb-8 overflow-hidden">
+    <section className="relative min-h-screen w-full bg-white flex flex-col pt-24 pb-8 overflow-hidden">
       
       {/* Background/Gradient */}
       <div className="absolute inset-0 bg-white z-0 pointer-events-none" />
 
       {/* Main Grid Container */}
-      <div className="relative z-10 w-full max-w-[1400px] mx-auto px-6 md:px-12 lg:px-20 h-full flex flex-col justify-end">
+      <div className="relative z-10 w-full max-w-[1400px] mx-auto px-6 md:px-12 lg:px-20 flex-1 flex flex-col justify-end">
         
         {/* Upper Setup: Image & Layout */}
-        <div className="flex-1 relative grid grid-cols-1 lg:grid-cols-12 gap-8 items-end h-full">
+        <div className="flex-1 relative grid grid-cols-1 lg:grid-cols-12 gap-8 lg:items-end lg:content-end h-full">
             
-            {/* Center Image - Absolute or Grid placed? 
-               To get the "text around image" look, precise positioning is best.
-               But responsive is key. 
-               Let's try: Image is centered. Text is in columns below/beside.
-            */}
-            
-             {/* The Image - Using CSS MASK for perfect fade */}
+            {/* The Image - Stacked at bottom on mobile, Absolute behind on Desktop */}
             <motion.div
                 initial={{ opacity: 0, scale: 0.95 }}
                 animate={{ opacity: 1, scale: 1 }}
                 transition={{ duration: 1 }}
-                className="lg:absolute lg:inset-x-0 lg:bottom-0 flex justify-center items-end pointer-events-none -z-10 h-[90%]"
+                className="relative h-[45vh] w-full order-3 lg:absolute lg:inset-x-0 lg:bottom-0 flex justify-center items-end pointer-events-none z-10 lg:-z-10 lg:h-[90%] lg:order-none mt-auto lg:mt-0"
             >
-                <div className="relative w-full max-w-lg lg:max-w-2xl h-full">
+                <div className="relative w-full max-w-sm md:max-w-lg lg:max-w-2xl h-full mx-auto">
                     {/* The Image Itself */}
-                    <div className="relative w-full h-full [mask-image:linear-gradient(to_bottom,black_45%,transparent_80%)]">
+                    <div className="relative w-full h-full [mask-image:linear-gradient(to_bottom,black_60%,transparent_98%)] lg:[mask-image:linear-gradient(to_bottom,black_50%,transparent_95%)]">
                          <Image 
                             src="/uploads/uploaded_image_0_1767545064999.png"
                             alt="Arifur Rahman"
                             fill
                             className="object-contain object-bottom"
                             priority
+                            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                         />
                     </div>
-                    
                 </div>
             </motion.div>
 
 
             {/* Left Content: Headline */}
-            <div className="lg:col-span-5 relative z-10 flex flex-col justify-end pb-12">
+            <div className="lg:col-span-5 relative z-10 flex flex-col justify-end pb-4 lg:pb-12 order-1 lg:order-none pt-4 lg:pt-0">
                 {/* Status Pill */}
                 <motion.div 
                     initial={{ opacity: 0, y: 20 }}
@@ -78,7 +72,7 @@ export default function NewHero() {
             <div className="hidden lg:block lg:col-span-2"></div>
 
             {/* Right Content: Description & Button */}
-            <div className="lg:col-span-5 relative z-10 flex flex-col justify-end items-start lg:pl-12 pb-12">
+            <div className="lg:col-span-5 relative z-10 flex flex-col justify-end items-start lg:pl-12 pb-8 lg:pb-12 order-2 lg:order-none">
                  <motion.p 
                     initial={{ opacity: 0, y: 30 }}
                     whileInView={{ opacity: 1, y: 0 }}
