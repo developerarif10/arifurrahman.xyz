f9f3eaf307044139174282a6ee0206f34953437b
Arifur Rahman <90108739+developerarif10@users.noreply.github.com>
2026-01-11 22:50:02 +0600

    update sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,25 +1,25 @@
 export default function sitemap() {
     return [
       {
-        url: 'https://arifurrahman.xyz',
+        url: 'https://arifurrahman.site',
         lastModified: new Date(),
         changeFrequency: 'yearly',
         priority: 1,
       },
       {
-        url: 'https://arifurrahman.xyz/about',
+        url: 'https://arifurrahman.site/about',
         lastModified: new Date(),
         changeFrequency: 'monthly',
         priority: 0.8,
       },
       {
-        url: 'https://arifurrahman.xyz/projects',
+        url: 'https://arifurrahman.site/projects',
         lastModified: new Date(),
         changeFrequency: 'weekly',
         priority: 0.8,
       },
       {
-        url: 'https://arifurrahman.xyz/contact',
+        url: 'https://arifurrahman.site/contact',
         lastModified: new Date(),
         changeFrequency: 'yearly',
         priority: 0.5,
